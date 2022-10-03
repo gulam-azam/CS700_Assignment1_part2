f57b30639b3e03cb747c212084d1844cfe8d148c
gulam-azam <114289229+gulam-azam@users.noreply.github.com>
2022-10-03 11:44:26 +0600

    Create header.h

---
+++ header.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<string>
+#include<fstream>
+#include<iomanip>
+#include <sstream>
+
+using namespace std;
+
+int* examScoresExt(string line,int n);
+string nameEx(string str, char del);
